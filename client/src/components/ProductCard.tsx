69566dd17b2b1675e41a7e47729e20d1cf48fbf3
blueface <azizwaadoud7@gmail.com>
2023-10-18 03:13:39 +0100

    feat(front-end): implement product details display

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { Product } from '../models/Product';
 import { useNavigate } from 'react-router-dom';
-import { Star, StarBorder, StarHalf } from '@mui/icons-material';
+import CardBody from './CardBody';
 
 interface Props {
   product: Product;
@@ -10,47 +10,10 @@ const ProductCard: React.FunctionComponent<Props> = (props) => {
   const { product } = props;
   const navigate = useNavigate();
 
-  const containedStars = (number: number) => {
-    const stars: ReactNode[] = [];
-
-    for (let i = 0; i < number; i++) {
-      stars.push(<Star fontSize="small" color={'warning'} />);
-    }
-
-    return stars;
-  };
-
-  const outlinedStars = (number: number) => {
-    const stars: ReactNode[] = [];
-
-    for (let i = 0; i < number; i++) {
-      stars.push(<StarBorder fontSize="small" color={'warning'} />);
-    }
-
-    return stars;
-  };
-
-  const renderRatingStars = (rate: number) => {
-    if (rate % 1 === 0) {
-      return containedStars(rate).concat(outlinedStars(5 - rate));
-    } else {
-      const ratingStars: ReactNode[] = containedStars(Math.trunc(rate));
-      ratingStars.push(<StarHalf fontSize="small" color={'warning'} />);
-      return ratingStars.concat(outlinedStars(4 - Math.trunc(rate)));
-    }
-  };
-
   return (
     <div className="card" onClick={() => navigate(`/products/${product.id}`)}>
-      <img src={product.image} alt="image " />
-      <div className="card-body">
-        <span>{product.title}</span>
-        <strong>{`${product.price} $`}</strong>
-        <div>
-          {renderRatingStars(product.rating.rate)}
-          <span>({product.rating.count})</span>
-        </div>
-      </div>
+      <img src={product.image} alt="image" />
+      <CardBody product={product} />
     </div>
   );
 };
--- client/src/components/ProductDetail.tsx
+++ client/src/components/ProductDetail.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ProductService } from '../services/productService';
+import CardBody from './CardBody';
 
 const ProductDetail: React.FunctionComponent = () => {
   const { productId } = useParams();
+  const [quantity, setQuantity] = useState<number>(1);
 
   const { isLoading, isError, data, error } =
     ProductService.getProductById(productId);
@@ -23,8 +25,41 @@ const ProductDetail: React.FunctionComponent = () => {
   }
 
   return (
-    <div className="Detail">
-      <strong>{data?.description}</strong>
+    <div className="product-details">
+      {data && (
+        <>
+          <div className="image-wrapper">
+            <img src={data?.image} alt="logo" />
+          </div>
+          <div className="details">
+            <CardBody product={data} />
+            <hr className="hr" />
+            <div className="category">
+              <span>Category: </span>
+              <strong>{data.category}</strong>
+            </div>
+            <div className="actions">
+              <span>Quantity: </span>
+              <div>
+                <button
+                  onClick={() => setQuantity(quantity - 1)}
+                  disabled={quantity === 1}
+                >
+                  -
+                </button>
+                <span>{quantity}</span>
+                <button onClick={() => setQuantity(quantity + 1)}>+</button>
+              </div>
+              <button className="btn">Add to cart</button>
+            </div>
+            <hr className="hr" />
+            <div>
+              <span>Description: </span>
+              <p className="description">{data.description}</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
